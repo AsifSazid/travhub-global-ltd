f8e1fb818b0935c3ffed6a73155e555092628c38
Asif Mostofa Sazid <asif83sazid@gmail.com>
2025-10-30 17:44:39 +0600

    custom routes added

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public const HOME = '/dashboard';
+
+    public function boot()
+    {
+        $this->routes(function () {
+            Route::prefix('api')  // ensure API prefix is correct
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
+                
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/package.php'));
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -45,38 +45,8 @@ Route::get('/inclusions/trash', [InclusionController::class, 'trash'])->name('in
 // Navigation
 Route::get('/navigations/list', [NavigationController::class, 'getData'])->name('navigations.getData');
 Route::get('/navigations/trash', [NavigationController::class, 'trash'])->name('navigations.trash');
-// Package
-Route::get('/packages/list', [PackageController::class, 'getData'])->name('packages.getData');
-Route::get('/packages/trash', [PackageController::class, 'trash'])->name('packages.trash');
-
-// Multi Form Start
-// Route::get('/packages/create-multi', function(){ return view('backend.packages.create-multistep'); })->name('packages.create');
-// Route::get('/packages/create', [PackageController::class, 'create'])->name('packages.create');
-
-
-// Route::post('/packages/step-1', [PackageController::class,'stepOne'])->name('packages.step1');
-// Route::post('/packages/step-2', [PackageController::class,'stepTwo'])->name('packages.step2');
-// Route::post('/packages/step-3', [PackageController::class,'stepThree'])->name('packages.step3');
-// Route::post('/packages/step-4', [PackageController::class,'stepFour'])->name('packages.step4');
-// Route::post('/packages/step-5', [PackageController::class,'stepFive'])->name('packages.step5');
-// Route::post('/packages/step-6', [PackageController::class,'stepSix'])->name('packages.step6');
-
-Route::get('/packages', [PackageController::class,'index'])->name('packages.index'); // list page
-
-Route::prefix('packages')->name('packages.')->group(function () {
-    Route::get('create', [PackageController::class, 'create'])->name('create');
-    Route::post('step/{step}', [PackageController::class, 'step'])->name('step');
-});
-
-Route::get('/api/countries/{id}/cities', [CountryController::class, 'getCities']);
-
-// Multi Form end
-
-
 // Role
 Route::get('/roles/list', [RoleController::class, 'getData'])->name('roles.getData');
-
-
 
 
 Route::get('/dashboard', function () {
@@ -148,10 +118,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/navigations/{id}/force-delete', [NavigationController::class, 'forceDelete'])->name('navigations.forceDelete');
     Route::get('/navigations/sync-routes', [NavigationController::class, 'syncRoutes'])
         ->name('navigations.syncRoutes');
-    // Package
-    Route::get('/packages/download/pdf', [PackageController::class, 'downloadPdf'])->name('packages.download.pdf');
-    Route::post('/packages/{id}/restore', [PackageController::class, 'restore'])->name('packages.restore');
-    Route::delete('/packages/{id}/force-delete', [PackageController::class, 'forceDelete'])->name('packages.forceDelete');
     // Role
     // Route::get('/roles/list', [RoleController::class, 'getData'])->name('roles.getData');
     Route::get('/roles/download/pdf', [RoleController::class, 'downloadPdf'])->name('roles.download.pdf');
