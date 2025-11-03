92f9562eb93882beb1a81875efbfe9161b085736
Asif Mostofa Sazid <asif83sazid@gmail.com>
2025-11-03 18:08:00 +0600

    unneccessary dark mode is removed

--- resources/views/components/backend/layouts/partials/navbar.blade.php
+++ resources/views/components/backend/layouts/partials/navbar.blade.php
@@ -34,7 +34,7 @@
         <!-- Desktop Right buttons -->
         <nav aria-label="Secondary" class="hidden space-x-2 md:flex md:items-center">
             <!-- Toggle dark theme button -->
-            <button aria-hidden="true" class="relative focus:outline-none" x-cloak @click="toggleTheme">
+            {{-- <button aria-hidden="true" class="relative focus:outline-none" x-cloak @click="toggleTheme">
                 <div class="w-12 h-6 transition rounded-full outline-none bg-primary-100 dark:bg-primary-lighter">
                 </div>
                 <div class="absolute top-0 left-0 inline-flex items-center justify-center w-6 h-6 transition-all duration-150 transform scale-110 rounded-full shadow-sm"
@@ -54,10 +54,10 @@
                             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
                     </svg>
                 </div>
-            </button>
+            </button> --}}
 
             <!-- Settings button -->
-            <button @click="openSettingsPanel"
+            {{-- <button @click="openSettingsPanel"
                 class="p-2 transition-colors duration-200 rounded-full text-primary-lighter bg-primary-50 hover:text-primary hover:bg-primary-100 dark:hover:text-light dark:hover:bg-primary-dark dark:bg-dark focus:outline-none focus:bg-primary-100 dark:focus:bg-primary-dark focus:ring-primary-darker">
                 <span class="sr-only">Open settings panel</span>
                 <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -67,7 +67,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-            </button>
+            </button> --}}
 
             <!-- User avatar button -->
             <div class="relative" x-data="{ open: false }">
