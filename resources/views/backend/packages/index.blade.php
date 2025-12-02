7cf546ddeb424bc5e6af85766b070a9d25080d38
AsifSazid <asif83sazi@gmail.com>
2025-12-02 20:50:28 +0600

    package index updated

--- resources/views/backend/packages/index.blade.php
+++ resources/views/backend/packages/index.blade.php
@@ -91,7 +91,7 @@
                                         <div class="flex justify-center">
                                             <a href="/admin/packages/${package.uuid}/show" class="px-1 text-blue-500 hover:text-blue-700" title="View"><i class="fas fa-eye"></i></a>
                                                 ${(package.status === 'inactive' && package.completion_status === 'incomplete')
-                                                    ? `<a href="/admin/packages/${package.uuid}/step/${package.progress_step ? package.progress_step + 1 : 1}" class="px-1 text-yellow-500 hover:text-yellow-700" title="Edit">
+                                                    ? `<a href="/admin/packages/${package.uuid}/step/${package.progress_step ? (package.progress_step + 1) : 1}" class="px-1 text-yellow-500 hover:text-yellow-700" title="Edit">
                                                         <i class="fas fa-edit"></i>
                                                     </a>`
                                                     : `<a href="/admin/packages/${package.uuid}/edit" class="px-1 text-yellow-500 hover:text-yellow-700" title="Edit">
